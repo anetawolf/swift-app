bd2609ea19c12f5b4506f1f61e572a1ff10c8e5a
Anetta <anetta@matys-Mac-Pro.local>
2017-04-12 20:41:32 +0200

    My first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBAction func buttonZelena(_ sender: Any) {
+        self.view.backgroundColor = UIColor.green
+        theLabel.textColor = UIColor.black
+        theLabel.text = "Zelena"
+    }
+    
+    @IBAction func buttonModra(_ sender: Any) {
+        self.view.backgroundColor = UIColor.blue
+        theLabel.textColor = UIColor.black
+        theLabel.text = "Modra"
+    }
+    
+    @IBAction func buttonFialova(_ sender: Any) {
+        self.view.backgroundColor = UIColor.purple
+        theLabel.textColor = UIColor.black
+        theLabel.text = "Fialova"
+    }
+    
+    @IBAction func buttonCervena(_ sender: Any) {
+        self.view.backgroundColor = UIColor.red
+        theLabel.textColor = UIColor.black
+        theLabel.text = "Cervena"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
